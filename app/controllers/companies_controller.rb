b8b828ece1d2e5545205737cb205aee4bf70af59
Billy <navyosu@gmail.com>
2015-07-18 01:09:03 -0600

    Adding a new company now works, not pretty, but what can you do.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -7,5 +7,22 @@ class CompaniesController < ApplicationController
   def show
     @company = Company.find(params[:id])
   end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to @company
+    else
+      render "new"
+    end
+  end
+
+  def company_params
+    params.require(:company).permit(:name, :url, :employees_count, :total_funding, :stage, :year_founded, :qualification)
+  end
   
 end
