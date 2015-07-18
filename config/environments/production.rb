ee2d94c3888797d2d5350224e2917faf78db667c
Billy <navyosu@gmail.com>
2015-07-18 01:22:30 -0600

    Hopefully get it to serve photos?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,8 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable serving static files from the `/public` folder by default since
-  config.serve_static_assets = true
+  # Apache or NGINX already handles this.
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
