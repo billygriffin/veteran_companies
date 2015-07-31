c253c53d2337fc79e772e91d6ec8fb0382a16272
Billy <navyosu@gmail.com>
2015-07-30 18:54:32 -0600

    Split out the main nav into a partial for each of the pages.

--- app/controllers/mission_controller.rb
+++ app/controllers/mission_controller.rb
@@ -2,7 +2,7 @@ class MissionController < ApplicationController
   respond_to :html
 
   def show
-    
+    @on_mission_page = true
   end
 
 end
--- app/controllers/research_controller.rb
+++ app/controllers/research_controller.rb
@@ -2,7 +2,7 @@ class ResearchController < ApplicationController
   respond_to :html
 
   def show
-
+    @on_research_page = true
   end
   
 end
